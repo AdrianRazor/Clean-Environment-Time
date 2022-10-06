35a417bc01d2426e2ff2476e1d61928edf3d907f
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-10-06 16:55:31 +0300

    created project

---
+++ task/img.js
@@ -0,0 +1,16 @@
+const { src, dest } = require("gulp");
+
+// Плагины
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+
+// Обработка Image
+const img = () => {
+  return src("./src/img/**/*.*")
+    .pipe(plumber())
+    .pipe(newer("./public/img"))
+    .pipe(dest("./public/img"));
+};
+
+module.exports = img;
